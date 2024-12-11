9f04f9fcd8d6982e3451bb5fdf8c4d19578ecf63
phyten <mikiyasu.obara@prominence-inc.co.jp>
2024-12-11 23:46:02 +0900

    feat: modify rubeet.rb to include new classes

--- lib/rubeet.rb
+++ lib/rubeet.rb
@@ -1,8 +1,26 @@
 # frozen_string_literal: true
 
 require_relative "rubeet/version"
+require_relative "rubeet/crawler"
+require_relative "rubeet/core"
 
+# module Rubeet
 module Rubeet
   class Error < StandardError; end
-  # Your code goes here...
+  class CrawlerError < Error; end
+  class ParseError < Error; end
+  class NetworkError < Error; end
+
+  class << self
+    attr_accessor :configuration
+
+    def configure
+      self.configuration ||= Configuration.new
+      yield(configuration) if block_given?
+    end
+
+    def reset
+      self.configuration = Configuration.new
+    end
+  end
 end
